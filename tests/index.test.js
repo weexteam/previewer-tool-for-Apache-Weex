a4cef2de9e347bbee42b8173eb99244c40ffd4ff
JackPu <kakashjack@gmail.com>
2017-02-09 19:15:29 +0800

    update ci tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,20 +6,22 @@ var fs = require('fs');
 var os = require('os');
 var expect = require('expect.js');
 
+const WEEX_TEMP = '.weex_tmp';
+const previewDir = pathTo.join(os.homedir(), WEEX_TEMP);
 
 fse.removeSync('./tests/dist/*');
-describe('test .vue file build', function () {
+describe('test main flow', function () {
   pre({
     entry: './tests/index.vue',
     temDir: './tests/dist',
     open: false
   });
   this.timeout(10000);
-  it('test .we file build', function (done) {
+  it('test .vue file build', function (done) {
     setTimeout(function () { 
-      expect(fs.existsSync('./tests/dist/app.js')).to.equal(true);
+      expect(fs.existsSync('./tests/dist/app.weex.js')).to.equal(true);
       done();
-    }, 4000);
+    }, 6000);
     
   });
 });
